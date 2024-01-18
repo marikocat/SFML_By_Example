dc50f7a2d79772fb49f9bf87988e9cc7092eaf1b
Maryna Ivanyk <ivanikmarina@gmail.com>
2024-01-18 14:53:55 +0200

    Chapter 09 ECS continued, all systems aded

---
+++ Chapter_09/01_ECS_Continued/EntityMessages.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class EntityMessage
+{
+	Move, Is_Moving, State_Changed, Direction_Changed,
+	Switch_State, Attack_Action, Dead
+};
---
+++ Chapter_09/01_ECS_Continued/Utilities.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#define RUNNING_WINDOWS
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace Utils
+{
+#ifdef RUNNING_WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <shlwapi.h>
+#include <pathcch.h>
+	inline std::string GetWorkingDirectory()
+	{
+		HMODULE hModule = GetModuleHandle(nullptr);
+		if (hModule)
+		{
+			char path[256];
+			GetModuleFileNameA(hModule, path, sizeof(path));
+			std::string s(path);
+			s = s.substr(0, s.find_last_of("\\/"));
+			return s;
+		}
+		return "";
+	}
+#elif defined RUNNING_LINUX
+#include <uinstd.h>
+	inline std::string GetWorkingDirectory()
+	{
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd)) != nullptr)
+		{
+			return std::string(cwd) + std::string("/");
+		}
+		return "";
+	}
+#endif // RUNNING_WINDOWS
+
+	inline std::string GetResourceDirectory()
+	{
+		//return GetWorkingDirectory() + std::string("\\Resources\\");
+		return GetWorkingDirectory() + std::string("\\Assets\\");
+	}
+}
